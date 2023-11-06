f3e65902187b07603fbf8a486ca7a7466d9f211f
Juli0931 <Juliib931@gmailcom>
2023-11-05 22:17:41 -0500

    Little change in the fetch logic

--- src/user/services/fetchPotter.js
+++ src/user/services/fetchPotter.js
@@ -3,10 +3,8 @@ const filterInput = document.getElementById('filterInput');
 filterInput.addEventListener('change', function () {
   const selectedValue = filterInput.value; 
 
-  // Construir la URL del endpoint dinámicamente
   const apiUrl = `https://api.potterdb.com/v1/${selectedValue}`;
 
-  // Realizar la solicitud fetch al endpoint correspondiente
   fetch(apiUrl)
     .then(response => {
       if (!response.ok) {
@@ -15,7 +13,6 @@ filterInput.addEventListener('change', function () {
       return response.json();
     })
     .then(data => {
-      // Trabajar con los datos devueltos
       console.log(`Datos de ${selectedValue}:`, data);
     })
     .catch(error => {
